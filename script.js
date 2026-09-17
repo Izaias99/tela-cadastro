aa31725d0d7805f0fb2ef50ece90c06fab726063
Izaias99 <isaiasmoreira.am@gmail.com>
2026-09-17 15:31:59 -0400

    feat: adiciona codigo completo do projeto de cadastro

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('cadForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  const nome = document.getElementById('nome').value;
+  const endereco = document.getElementById('endereco').value;
+  
+  const li = document.createElement('li');
+  li.innerHTML = `<strong>${nome}</strong><br><small>${endereco}</small>`;
+  
+  document.getElementById('listaClientes').appendChild(li);
+  
+  document.getElementById('cadForm').reset();
+});
