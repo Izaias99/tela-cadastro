f27ebe8748b02ee7a5de0316a80ae70bf3401f26
Izaias99 <isaiasmoreira.am@gmail.com>
2026-09-17 15:46:37 -0400

    feat: insere conteudo dos arquivos via Set-Content

---
+++ README.md
@@ -0,0 +1,5 @@
+# TechManaus - Sistema de Cadastro de Clientes
+## Recomendação de Nuvem
+Utilização da AWS em modelo PaaS para alta disponibilidade e facilidade de deploy.
+## Ciclo DevOps
+Iterações com Git, branches dev/main e simulação de automação de entrega contínua.
--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-document.getElementById('cadForm').addEventListener('submit', function(e) {
+document.getElementById("cadForm").addEventListener("submit", function(e) {
   e.preventDefault();
-  
-  const nome = document.getElementById('nome').value;
-  const endereco = document.getElementById('endereco').value;
-  
-  const li = document.createElement('li');
+  const nome = document.getElementById("nome").value;
+  const endereco = document.getElementById("endereco").value;
+  const li = document.createElement("li");
   li.innerHTML = `<strong>${nome}</strong><br><small>${endereco}</small>`;
-  
-  document.getElementById('listaClientes').appendChild(li);
-  
-  document.getElementById('cadForm').reset();
+  document.getElementById("listaClientes").appendChild(li);
+  document.getElementById("cadForm").reset();
 });
